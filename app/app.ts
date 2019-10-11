4b9c81a10a25413be41077db5d6a62b181a0b323
Stephen O'Connor <stephen.oconnor@collectivworks.com>
2019-10-11 13:28:35 +0100

    Added type annotations.

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,28 @@
-function startGame() {
+function startGame(): void {
   // starting a new game.
-  const messagesElement = document.getElementById('messages');
-  messagesElement!.innerText = 'Welcome to MultiMath! Starting a new game...';
+  const playerName: string | undefined = getInputValue('playername');
+  logPlayer(playerName);
+
+  postScore(100, playerName);
+}
+
+function logPlayer(name: string = 'MultiMath Player'): void {
+  console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementId: string): string | undefined {
+  const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementId);
+
+  if(inputElement.value === '') {
+    return undefined;
+  } else {
+    return inputElement.value;
+  }
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+  const scoreElement: HTMLElement | null = document.getElementById('postedScores');
+  scoreElement!.innerText = `${score} - ${playerName}`;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
