c3a7a5b4ac4e26190160438c0410bad8f39785c7
Stephen O'Connor <stephen.oconnor@collectivworks.com>
2019-10-11 13:40:39 +0100

    Update types.

--- app/app.ts
+++ app/app.ts
@@ -4,6 +4,7 @@ function startGame(): void {
   logPlayer(playerName);
 
   postScore(100, playerName);
+  postScore(-5, playerName)
 }
 
 function logPlayer(name: string = 'MultiMath Player'): void {
@@ -21,8 +22,24 @@ function getInputValue(elementId: string): string | undefined {
 }
 
 function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+  let logger: (value: string) => void;
+
+  if(score < 0) {
+    logger = logError
+  } else {
+    logger = logMessage;
+  }
+
   const scoreElement: HTMLElement | null = document.getElementById('postedScores');
   scoreElement!.innerText = `${score} - ${playerName}`;
+
+  logger(`Score: ${score}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
+
+const logMessage = (message: string) => console.log(message);
+
+function logError(err: string): void {
+  console.error(err);
+}
